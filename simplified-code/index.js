52302480b9ca3ef39e1f9142523e30688e179d47
Prince781 <princetonf@optonline.net>
2014-02-26 10:44:23 -0500

    Updating.

--- simplified-code/index.js
+++ simplified-code/index.js
@@ -1,7 +1,58 @@
 // index.js
-// for the main page
+// for the main page of BCAMart
+
+"use strict";
+
+var Choices = new function() {
+	// private
+	var _ = this;
+	var bar;
+	
+	this.init = function() {
+		bar = $("#slide2 > .sl_holder > .sl_trapdoor");
+	};
+	
+	this.show = new function() {
+		// private
+		var _s = this;
+		
+		function showBar() {
+			if (bar.css("height") != "0px") return;
+			bar.css("display", "block");
+			bar.css("height", "223px");
+		}
+		
+		function hideBar() {
+			if (bar.css("height") == "0px") return;
+			bar.css("height", "0px");
+			setTimeout(function() { bar.css("display", "none"); });
+		}
+		
+		/* show our food choices */
+		this.food = function() {
+			showBar();
+		};
+		
+		/* show our drinks */
+		this.drinks = function() {
+		};
+		
+		/* show our available clothing */
+		this.clothing = function() {
+		};
+		
+		/* show our supplies */
+		this.supplies = function() {
+		};
+	}
+};
 
 window.onload = function() {
-	// initialize buttons for slide 2
+	Choices.init(); // initialize choices
 	
+	// initialize buttons for slide 2
+	var buttons = $("#slide2 .sl_choice > *:first-child");
+	for (var i=0, name=""; i<buttons.size(); i++)
+		if (typeof (name=$(buttons[i]).parent().attr("name")) != "undefined")
+			buttons[i].onclick = Choices.show[name];
 };
